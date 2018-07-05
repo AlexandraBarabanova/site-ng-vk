395ddaff0d6068134ef4275d6357a2ff4fa4248e
AlexandraBarabanova <sashkamasharova@gmail.com>
2018-07-06 01:36:26 +0300

    +

--- src/app/shared/app.service.ts
+++ src/app/shared/app.service.ts
@@ -1,4 +1,4 @@
-import { Http } from '@angular/http';
+import { Http, Headers, RequestOptions } from '@angular/http';
 import { Injectable} from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/toPromise';
@@ -16,8 +16,13 @@ export class AppService {
 
         constructor(private http: Http) {}
         public getFriends(): Promise<any> {
+            let myHeaders = new Headers({
+                'Content-Type': 'application/json',
+                'X-MyHeader': 'Hello'
+            });
+            let options = new RequestOptions({ headers: myHeaders});
             return this.http.get('https://api.vk.com/method/friends.get?user_id=' + localStorage.getItem('user_id') +
-                                        '&v=5.52&access_token=' + localStorage.getItem('access_token'))
+                                        '&v=5.52&access_token=' + localStorage.getItem('access_token'), options)
                 .toPromise()
                 .then(res => res.json().data)
                 .then(frined => this.frined = frined)
