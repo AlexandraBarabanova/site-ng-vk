a99510c7aa87643fbbabc81c449dac47d124e874
AlexandraBarabanova <sashkamasharova@gmail.com>
2018-07-05 23:00:10 +0300

    +

--- src/app/friends/friends.module.ts
+++ src/app/friends/friends.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-// import { FriendsRoutingModule } from './friends-routing.module';
+import { FriendsRoutingModule } from './friends-routing.module';
 import { FriendsComponent } from './friends.component';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    // FriendsRoutingModule
+    FriendsRoutingModule
   ],
   declarations: [FriendsComponent]
 })
