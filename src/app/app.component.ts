8136fa6f3ae526db97d3f61247c75a4f0154870a
AlexandraBarabanova <sashkamasharova@gmail.com>
2018-07-06 00:02:44 +0300

    +

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,18 @@ import { Router } from '@angular/router';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
     constructor(private router: Router) {
+        const access_token: string = '#access_token=';
+        const user_id: string = 'user_id=';
         let path: string = window.location.hash;
-        console.log('path', path, this.router);
+        if (path.indexOf(access_token)) {
+            localStorage.setItem('access_token', path.substring(path.indexOf(access_token)+access_token.length, path.indexOf('&')));
+            localStorage.setItem('user_id', path.substring(path.indexOf(user_id)+user_id.length, path.length));
+            path = '#/friends';
+        }
+        console.log('test', localStorage.getItem('access_token'), localStorage.getItem('user_id'));
         if (path && path.length > 0) {
             this.router.navigate([path.substr(2)]);
         }
