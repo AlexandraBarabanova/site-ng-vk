cf294da7e2b9cc31cde5d19a0d33a43162629c84
AlexandraBarabanova <sashkamasharova@gmail.com>
2018-07-05 22:57:05 +0300

    +

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,14 +1,14 @@
-// import { RouterModule, Routes} from '@angular/router';
-// import { NgModule } from '@angular/core';
-// import { LoginComponent} from "./login.component";
-//
-// const routes: Routes = [
-//   {   path: 'login', component: LoginComponent}
-// ];
-//
-// @NgModule({
-//   imports: [RouterModule.forChild(routes)],
-//   exports: [RouterModule]
-// })
-//
-// export class LoginRoutingModule {}
+import { RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { LoginComponent} from "./login.component";
+
+const routes: Routes = [
+  {   path: 'login', component: LoginComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class LoginRoutingModule {}
