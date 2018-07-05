bff7624756e926cbc45c6b16ed410e921b90f09d
AlexandraBarabanova <sashkamasharova@gmail.com>
2018-07-06 01:13:46 +0300

    +

--- src/app/shared/app.service.ts
+++ src/app/shared/app.service.ts
@@ -1,4 +1,4 @@
-import { Http, Headers } from '@angular/http';
+import { Http } from '@angular/http';
 import { Injectable} from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/toPromise';
@@ -16,11 +16,7 @@ export class AppService {
 
         constructor(private http: Http) {}
         public getFriends(): Promise<any> {
-            const headers = new Headers();
-            headers.append(':method', 'GET');
-            headers.append(':path', '/method/friends.getOnline?v=5.52&access_token=35c9e33d5a4c871e7294da17d0fe100966cae076b11e2c5f0de79883e1f4f76b196d66eb65b14fedf6887');
-            headers.append(':scheme', 'https');
-            return this.http.get('/method/friends.getOnline?v=5.52&access_token=35c9e33d5a4c871e7294da17d0fe100966cae076b11e2c5f0de79883e1f4f76b196d66eb65b14fedf6887', {headers: headers})
+            return this.http.get('/method/friends.getOnline?v=5.52&access_token=35c9e33d5a4c871e7294da17d0fe100966cae076b11e2c5f0de79883e1f4f76b196d66eb65b14fedf6887')
                 .toPromise()
                 .then(res => res.json().data)
                 .then(frined => this.frined = frined)
