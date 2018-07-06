964810b85304534be0b25a743680598494392650
AlexandraBarabanova <sashkamasharova@gmail.com>
2018-07-06 15:07:19 +0300

    +

--- src/app/shared/app.service.ts
+++ src/app/shared/app.service.ts
@@ -18,14 +18,14 @@ export class AppService {
         constructor(private http: Http) {}
         public getFriends(): any {
             const headers = new Headers();
-            headers.append('Access-Control-Allow-Headers', 'Content-Type');
-            // headers.append('Content-Type', 'application/json');
-            // headers.append('Access-Control-Allow-Methods', 'GET');
+            // headers.append('Access-Control-Allow-Headers', 'Content-Type');
+            headers.append('Content-Type', 'application/json');
+            headers.append('Access-Control-Allow-Methods', 'GET');
             headers.append('Access-Control-Allow-Origin', '*');
             // headers.append('Api-User-Agent', 'Example/1.0');
-            // const options = new RequestOptions({ headers: headers});
+            const options = new RequestOptions({ headers: headers});
             return this.http.get('https://api.vk.com/method/friends.get?user_id=' + localStorage.getItem('user_id') +
-                                        '&v=5.52&access_token=' + localStorage.getItem('access_token'))
+                                        '&v=5.52&access_token=' + localStorage.getItem('access_token'), options)
                 .subscribe((data: any) => {this.frined = data; });
         }
         // private handleError(error: any) {
