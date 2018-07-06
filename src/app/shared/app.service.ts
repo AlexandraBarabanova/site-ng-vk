337946ee3ea79954575a3e3320d3265bee2d63f3
AlexandraBarabanova <sashkamasharova@gmail.com>
2018-07-06 15:51:56 +0300

    +

--- src/app/shared/app.service.ts
+++ src/app/shared/app.service.ts
@@ -21,11 +21,11 @@ export class AppService {
             // headers.append('Access-Control-Allow-Headers', 'Content-Type');
             // headers.append('Content-Type', 'application/json');
             // headers.append('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-            // headers.append('Access-Control-Allow-Origin', 'https://vk-oauth-ng.herokuapp.com');
+            headers.append('Access-Control-Allow-Origin', 'https://api.vk.com');
             // headers.append('Api-User-Agent', 'Example/1.0');
-            // const options = new RequestOptions({ headers: headers});
+            const options = new RequestOptions({ headers: headers});
             return this.http.get('https://api.vk.com/method/friends.get?user_id=' + localStorage.getItem('user_id') +
-                                        '&v=5.52&access_token=' + localStorage.getItem('access_token'))
+                                        '&v=5.52&access_token=' + localStorage.getItem('access_token'), options)
                 .subscribe((data: any) => {this.frined = data; });
         }
         // private handleError(error: any) {
