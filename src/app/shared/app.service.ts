54d62bd0ce6b22e464ca8527f79930879ce458f9
AlexandraBarabanova <sashkamasharova@gmail.com>
2018-07-06 16:44:32 +0300

    +

--- src/app/shared/app.service.ts
+++ src/app/shared/app.service.ts
@@ -22,7 +22,7 @@ export class AppService {
 
             // return this.http.jsonp('https://api.vk.com/method/friends.get?user_id=' + localStorage.getItem('user_id') +
             //                             '&v=5.52&access_token=' + localStorage.getItem('access_token'), 'callback');
-            return this.jsonp.get('https://api.vk.com/method/friends.get?user_id=' + localStorage.getItem('user_id') +
+            return this.jsonp.request('https://api.vk.com/method/friends.get?user_id=' + localStorage.getItem('user_id') +
                 '&v=5.52&access_token=' + localStorage.getItem('access_token') + '&callback=JSONP_CALLBACK')
                 .map(function(res: Response) {
                     console.log('fsdgbdf', res);
